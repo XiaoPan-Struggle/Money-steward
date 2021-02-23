4d6939c87fde86280fcf0fa04d576207e290003b
XiaoPan <2489964425@qq.com>
2021-02-23 18:24:18 +0800

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import styled from 'styled-components';
-import Button from './components/Button';
-import Box from './components/Box';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Box>
-        <Button>你好</Button>
-      </Box>
+      hello
     </div>
   );
 }
