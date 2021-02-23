a5e7f623c6c3092688c3b1c13d14167d513edadd
XiaoPan <2489964425@qq.com>
2021-02-23 19:40:56 +0800

    完成 nav 切换栏

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,67 @@
+import Nav from 'components/Nav';
 import React from 'react';
-import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
 
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
 
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+
+        <Nav />
+      </Wrapper>
+    </Router>
   );
+}
+function NoMatch() {
+  return <h2>404</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记一笔页面</h2>;
 }
 
 export default App;
