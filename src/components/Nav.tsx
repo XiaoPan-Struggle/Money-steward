ede2f907e852dd6015f2460dba5c4faf40de8fe9
XiaoPan <2489964425@qq.com>
2021-02-23 21:52:09 +0800

    icons 引入

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg');
+require('icons/tags.svg');
+require('icons/statistics.svg');
+
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -11,6 +16,14 @@ const NavWrapper = styled.nav`
       width: 33.33%;
       text-align: center;
       padding: 16px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      > .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `
@@ -20,12 +33,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tags" />
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记一笔</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics" />
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
