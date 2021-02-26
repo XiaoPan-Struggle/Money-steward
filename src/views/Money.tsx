ac210ee1c4346deeea203b6d6ec88911f6084c6a
XiaoPan <2489964425@qq.com>
2021-02-26 18:32:13 +0800

    重构表单数据搜集

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout';
 import React, {useState} from 'react';
 import styled from 'styled-components';
-import { TagsSection } from './Money/TagsSection';
+import {TagsSection} from './Money/TagsSection';
 import {CategorySection} from './Money/CategorySection';
 import {NoteSection} from './Money/NoteSection';
 import {NumberPadSection} from './Money/NumberPadSection';
@@ -9,43 +9,36 @@ import {NumberPadSection} from './Money/NumberPadSection';
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
-`
-
+`;
 type Category = '-' | '+'
 function Money() {
-  const [selected,setSelected] = useState({
+  const [selected, setSelected] = useState({
     tags: [] as string[],
     note: '',
     category: '-' as Category,
     amount: 0
-  })
+  });
+  const onChange = (obj: Partial<typeof selected>) => {
+    setSelected({
+      ...selected,
+      ...obj
+    });
+  };
   return (
     <MyLayout>
       <TagsSection value={selected.tags}
-                   onChange={tags => setSelected({
-                     ...selected,
-                     tags: tags
-                   })}/>
+                   onChange={tags => onChange({tags})}/>
       <NoteSection value={selected.note}
-                    onChange={note => setSelected({
-                        ...selected,
-                        note:note
-                      }
-                    )}/>
+                   onChange={note => onChange({note})}/>
       <CategorySection value={selected.category}
-                        onChange={category => setSelected({
-                          ...selected,
-                          category: category
-                        })}/>
+                       onChange={category => onChange({category})}/>
       <NumberPadSection value={selected.amount}
-                        onChange={amount => setSelected({
-                          ...selected,
-                          amount:amount
-                        })}
+                        onChange={amount => onChange({amount})}
                         onOk={() => {
                           console.log(selected);
                         }}/>
     </MyLayout>
   );
 }
-export default Money
+
+export default Money;
