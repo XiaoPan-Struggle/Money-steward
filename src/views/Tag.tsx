8c4aa65d2efc98fb903346400d65a58a716d5d87
XiaoPan <2489964425@qq.com>
2021-02-27 13:23:21 +0800

    Tag.tsx 里 Topper UI

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -2,22 +2,38 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import {useTags} from 'useTags';
 import Layout from 'components/Layout';
-import Icon from '../components/Icon';
+import Icon from 'components/Icon';
 import {Button} from './Tags/UI';
+import styled from 'styled-components';
 
 type Params = {
   id: string
 }
+const Topper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background: #f2c5aa;
+  > .icon{
+    width: 20px;
+    height: 20px;
+    fill: #707175;
+  }
+`
+
 const Tag:React.FC = () => {
   const {findTag} = useTags()
   let {id} = useParams<Params>();
   const tag = findTag(parseInt(id))
   return (
     <Layout>
-      <header>
+      <Topper>
         <Icon name="left" />
         <span>编辑标签</span>
-      </header>
+        <Icon/>
+      </Topper>
       <div>
         <label>
           <span>标签名</span>
