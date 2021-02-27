e3e496aeb8ca0ebfee0385df2921b01c584b9008
XiaoPan <2489964425@qq.com>
2021-02-27 11:44:52 +0800

    解决 Id 多次初始化BUG

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,13 +1,14 @@
 import {useState} from 'react';
 import {createId} from 'lib/createId';
 
+const defaultId = [
+  {id: createId(), name: '衣'},
+  {id: createId(), name: '食'},
+  {id: createId(), name: '住'},
+  {id: createId(), name: '行'}
+]
 const useTags = () => {
-  const [tags, setTags] = useState<{id:number,name:string}[]>([
-    {id: createId(), name: '衣'},
-    {id: createId(), name: '食'},
-    {id: createId(), name: '住'},
-    {id: createId(), name: '行'}
-  ]);
+  const [tags, setTags] = useState<{id:number,name:string}[]>(defaultId);
   return {tags, setTags};
 };
 export {useTags};
